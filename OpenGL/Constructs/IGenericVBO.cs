a65e7100b33350c6be9c9bf80c5526dcfe3c9d4f
giawa <giawa@hotmail.com>
2019-01-28 14:29:18 -0800

    Merge pull request #25 from TheAIBot/add-GenericVBO-constructor

---
+++ OpenGL/Constructs/IGenericVBO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenGL
+{
+    public interface IGenericVBO : IDisposable
+    {
+        string Name { get; }
+        VertexAttribPointerType PointerType { get; }
+        int Length { get; }
+        BufferTarget BufferTarget { get; }
+        uint ID { get; }
+        int Size { get; }
+        uint Divisor { get; }
+        bool Normalize { get; }
+        bool CastToFloat { get; }
+        bool IsIntegralType { get; }
+        uint vboID { get; }
+    }
+}
